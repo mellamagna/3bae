6c0881dcec0cd730f73f6bafc68d3721ad794b5f
Mel Lamagna <mel@mellamagna.com>
2020-11-13 14:23:36 -0500

    navbar fixes

--- src/App.js
+++ src/App.js
@@ -19,8 +19,16 @@ function App() {
 
 
         <div className="container-fluid" id="headernavbar">
-          <nav>
+          
+          <nav id="headernavbarlinks">
             <ul className="nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/"><img src="3bae.svg" height="50" alt="3bae" id="logo"/></Link>
+              </li>
+              <li className="nav-item">
+                <span id="slogan">We're Like eBay, But With a Three&trade;</span>
+              </li>
+              <li className="vl"></li>
               <li className="nav-item">
                 <Link className="nav-link" to="/">Home</Link>
               </li>
