a35f90cf4817609d457915b40ec1961f5c094c92
Mel Lamagna <mel@mellamagna.com>
2020-11-13 14:31:01 -0500

    split navbar into separate component

--- src/App.js
+++ src/App.js
@@ -11,35 +11,14 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import HeaderNavbar from './components/HeaderNavbar';
 
 function App() {
   return (
     <Router>
       <div>
 
-
-        <div className="container-fluid" id="headernavbar">
-          <nav>
-            <ul className="nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/"><img src="3bae.svg" height="50" alt="3bae" id="logo"/></Link>
-              </li>
-              <li className="nav-item">
-                <span id="slogan">We're Like eBay, But With a Three&trade;</span>
-              </li>
-              <li className="vl"></li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/listings">Listings</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/cart">Cart</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <HeaderNavbar />
 
         <Switch>
           <Route path="/listings">
---
+++ src/components/HeaderNavbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const HeaderNavbar = () => {
+	return (
+		<div className="container-fluid" id="headernavbar">
+          <nav>
+            <ul className="nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/"><img src="3bae.svg" height="50" alt="3bae" id="logo"/></Link>
+              </li>
+              <li className="nav-item">
+                <span id="slogan">We're Like eBay, But With a Three&trade;</span>
+              </li>
+              <li className="vl"></li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/listings">Listings</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+	);
+};
+
+export default HeaderNavbar;
