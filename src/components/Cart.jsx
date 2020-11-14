509cc1cc707395b37e21ce7ed1a0eae37e46eb7b
Mel Lamagna <mel@mellamagna.com>
2020-11-14 00:29:13 -0500

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import CartListing from './CartListing';
 
 const Cart = props => {
 	return (
 		<div className="container">
-			<h1>Your Cart</h1>
+			<h1>Listings</h1>
+			<table cellPadding="10">
+				<tr>
+					<th>ID</th>
+					<th>Image</th>
+					<th>Item&nbsp;Name / Description</th>
+					<th>Actions</th>
+				</tr>
+				{props.products.map(item => {
+					return(
+						<CartListing 
+							id={item.id}
+							name={item.name}
+							image={item.image}
+							price={item.price}
+							desc={item.desc}
+						/>
+					)
+				})}
+			</table>
 		</div>
 	);
 };
